804ac5681d6577bf23684a972b6614c05fbca8c1
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-29 17:05:29 -0300

    Adicionando Exe03

---
+++ exe01/Exe03.java
@@ -0,0 +1,28 @@
+package giuliaebruna;
+
+import java.util.Scanner;
+
+public class Exe03 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int num;
+		int soma;
+
+		System.out.println("Informe um numero: ");
+		num = scanner.nextInt();
+		
+		if (num % 2 == 0) {
+			soma = (num * (num + 1)) / 2;
+			System.out.println("A soma dos numeros inteiro de 1 a " + num + " é: " + soma);
+		}
+
+		else {
+			System.out.println("o numero informado não é impar.");
+		}
+
+		scanner.close();
+	}
+
+}
