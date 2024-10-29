66962f96b0ca12ded1eca521232eb94becc23d1a
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-29 17:28:20 -0300

    Atualizando Exe03

--- exe01/Exe03.java
+++ exe01/Exe03.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Scanner;
 
 public class Exe03 {
@@ -8,21 +6,20 @@ public class Exe03 {
 		Scanner scanner = new Scanner(System.in);
 
 		int num;
-		int soma;
 
 		System.out.println("Informe um numero: ");
 		num = scanner.nextInt();
-		
-		if (num % 2 == 0) {
-			soma = (num * (num + 1)) / 2;
-			System.out.println("A soma dos numeros inteiro de 1 a " + num + " é: " + soma);
-		}
 
-		else {
-			System.out.println("o numero informado não é par.");
-		}
+		int soma = 0;
 
-		scanner.close();
+		for (int i = 1; i <= num; i++) {
+			if (i % 2 == 0) {
+				soma += i;
+			}
+		}
+       
+		System.out.println("As somas dos numeros pares ente 1 e " + num + " é: " + soma);
+
 	}
 
 }
